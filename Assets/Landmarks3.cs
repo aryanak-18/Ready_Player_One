4818c1e8f60c865b35a6a241e744477b024c1995
aryanak-18 <113026829+aryanak-18@users.noreply.github.com>
2023-06-25 00:35:40 +0530

    Add files via upload

---
+++ Assets/Landmarks3.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//listener 2.0
+public class Landmarks3 : MonoBehaviour
+{
+    public Listener2 listener;
+
+    int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = int.Parse(gameObject.name.Split("(")[1].Split(")")[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         fetchHandLandmarks();
+        //fetchFaceLandmarks();
+
+    }
+
+    void fetchHandLandmarks()
+    {
+        if (listener != null)
+        {
+            var pos = listener.landmarkList;
+            if (pos.Capacity != 0 && num < pos.Capacity)
+            {
+                transform.position = new Vector3(pos[num].x*-5,pos[num].y*-5,pos[num].z*-5-4);
+            }
+        }
+
+    }
+
+    
+}
